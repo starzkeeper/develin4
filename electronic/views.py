5f32bb2762ac8e64f61538576dc3af17347a6983
starzkeeper <yaroslav020304@gmail.com>
2022-12-20 14:22:21 +0300

    second commit

--- electronic/views.py
+++ electronic/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
-from rest_framework import generics,viewsets
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .models import Product, Category
-from .permissions import IsAdminOrRead
 from .serializers import ProductSerializer
 
 class ProductPagination(PageNumberPagination):
